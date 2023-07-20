3aa1956eae6d5b14d1156b594146bfdcff783ef6
VyVy99 <dinhhavy2406@gmail.com>
2023-07-20 10:57:47 +0700

    first

---
+++ NhanVien.js
@@ -0,0 +1,44 @@
+function NhanVien(tknv, name, email, password, datepicker, luongCB, chucvu, gioLam) {
+    this.tknv = "";
+    this.name = "";
+    this.email = "";
+    this.password = "";
+    this.datepicker = "";
+    this.luongCB = "";
+    this.chucvu = "";
+    this.gioLam = "";
+
+    // phương thức: tính tổng lương và xếp loại nhân viên
+    this.tinhLuong = function () {
+        var tongLuong = 0;
+        if (this.chucvu == "Giám đốc") {
+            tongLuong = this.luongCB * 3;
+        } else if (this.chucvu === 'Trưởng phòng') {
+            tongLuong = this.luongCB * 2;
+        } else if (this.chucvu == "Nhân viên") {
+            tongLuong = this.luongCB;
+        }
+        return tongLuong;
+    }
+
+    this.xepLoaiNhanVien = function () {
+        var xepLoai = "";
+        if (this.gioLam >= 192) {
+            xepLoai = "Nhân viên xuất sắc";
+        } else if (this.gioLam >= 176) {
+            xepLoai = "Nhân viên giỏi";
+        } else if (this.gioLam >= 160) {
+            xepLoai = "Nhân viên khá";
+        } else if (this.gioLam < 160) {
+            xepLoai = "Nhân viên trung bình";
+        }
+        return xepLoai;
+    }
+}
+
+
+//
+
+
+// xây dựng tổng lương và xếp loại nhân viên
+
--- data/index.js
+++ data/index.js
@@ -1,22 +1,64 @@
-function NhanVien(tknv, name, email, password, datepicker, luongCB, chucvu, gioLam) {
-    this.tknv = "";
-    this.name = "";
-    this.email = "";
-    this.password = "";
-    this.datepicker = "";
-    this.luongCB = "";
-    this.chucvu = "";
-    this.gioLam = "";
+document.getElementById('btnThem').onclick = function () { 
+var arrIdInput = [
+    "tknv", "name", "email", "password", "datepicker", "luongCB", "chucvu", "gioLam",
+];
+// tạo 1 mảng ở ngoài để chứa
 
-    // phương thức: tính tổng lương và xếp loại nhân viên
-    this.tinhTongLuong = function () {
+var arrNhanVien = [];
+// phương thức thêm nhân viên
+function themNhanVien(event) {
 
-
+    event.preventDefault();
+    console.log('submit');
+    var nhanVien = new NhanVien();
+    for (var i = 0; i < arrIdInput.length; i++) {
+        var value = document.getElementById(arrIdInput[i]).value;
+        nhanVien[arrIdInput[i]] = value;
 
     }
+    console.log(nhanVien);
+    // ta tính lương và xếp loaij
+    var tongLuong = this.tinhLuong();
+    var xepLoai = this.xepLoaiNhanVien();
+    // them thông tin vào mảng nhân viên
+    arrNhanVien.push(nhanVien);
+    console.log(arrNhanVien);
+    // cập nhật lại danh sách sinh viên
+
+    hienThiDanhSachNhanVien()
+
+    // đóg form thêm mới 
 
 
 
-};
-// xây dựng tổng lương và xếp loại nhân viên
+}
+
+function hienThiDanhSachNhanVien(arrNhanVien) {
+    var content = "";
+    for (var i = 0; i < arrNhanVien.length; i++) {
+        var nv = arrNhanVien[i];
+        content += `
+            <tr>
+                <td>${nhanVien.tknhanVien}</td>
+                <td>${nhanVien.name}</td>
+                <td>${nhanVien.email}</td>
+                <td>${nhanVien.password}</td>
+                <td>${nhanVien.datepicker}</td>
+                <td>${nhanVien.luongCB}</td>
+                <td>${nhanVien.chucvu}</td>
+                <td>${nhanVien.tinhLuong()}</td>
+                <td>${nhanVien.gioLam}</td>
+                <td>${nhanVien.xepLoaiNhanVien()}</td>
+                <td>
+                    <button class="btn btn-danger" onclick="xoaNhanVien('${nhanVien.tknv}')">Xóa</button>
+                    <button class="btn btn-primary" onclick="suaNhanVien('${nhanVien.tknv}')">Sửa</button>
+                </td>
+            </tr>
+        `;
+    }
+    document.getElementById("tableDanhSach").innerHTML = content;
+}
+
+
+}
 
